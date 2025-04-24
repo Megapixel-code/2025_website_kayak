2bf34f8cb1f73187b81a2436b36d721585e863b8
Megapixel-code <chainemegapixel@gmail.com>
2025-04-24 20:20:20 +0200

    begining of the home page

--- website/main_app/views.py
+++ website/main_app/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("hello world")
+def home(request):
+    return render(request, "le_club.html", {})
 
 
