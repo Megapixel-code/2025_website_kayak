8a1b45bed335098c5835207956d91968420a951f
Megapixel-code <chainemegapixel@gmail.com>
2025-04-25 17:02:04 +0200

    progress on the top bar

--- website/main_app/static/js/script.js
+++ website/main_app/static/js/script.js
@@ -1,10 +1,10 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction(){
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50){
-        document.getElementById("header").style.background = "black";
+    if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5){
+        document.getElementById("header").style.background = `rgba(256, 256, 256, 1)`;
     }
     else {
-        document.getElementById("header").style.background = "yellow";
+        document.getElementById("header").style.background = `rgba(256, 256, 256, 0)`;
     }
 }
