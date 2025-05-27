0dd8e7188dab10800bc8fa9a1a8501e94fc040b6
Megapixel-code <chainemegapixel@gmail.com>
2025-05-27 22:47:08 +0200

    header change and file organisation change

---
+++ website/main_app/models/content.py
@@ -0,0 +1,4 @@
+from django.db import models
+
+class page (models.Model):
+    pass
--- website/main_app/static/js/script.js
+++ website/main_app/static/js/script.js
@@ -3,8 +3,10 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction(){
     if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5){
         document.getElementById("header").style.background = `rgba(256, 256, 256, 1)`;
+        document.getElementById("header").style.boxShadow = `0px 1px 5px gray`;
     }
     else {
         document.getElementById("header").style.background = `rgba(256, 256, 256, 0.1)`;
+        document.getElementById("header").style.boxShadow = `0px 1px 5px transparent`;
     }
 }
