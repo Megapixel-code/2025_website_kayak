304e286cd3e44489b162395cd80b36605b107896
Megapixel-code <chainemegapixel@gmail.com>
2025-05-30 17:13:45 +0200

    header correct remake and js changes (resizefunction)

--- website/main_app/static/js/script.js
+++ website/main_app/static/js/script.js
@@ -1,16 +1,38 @@
-window.onscroll = function() {scrollFunction()};
+window.onresize = function() {resizeFunction()}
+
+window.onload = resizeFunction
+
+
+function resizeFunction(){
+    if (document.body.clientWidth > 900){
+        window.onscroll = function() {scrollFunction()};
+        scrollFunction();
+    }
+    else{
+        window.onscroll = function() {};
+
+        var header = document.getElementById("header");
+        header.style.background = '';
+        header.style.boxShadow = '';
+        header.style.color = '';
+        document.querySelector("header#header div ul").style.color = '';
+    }
+}
+
 
 function scrollFunction(){
+    var header = document.getElementById("header");
+    
     if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5){
-        document.getElementById("header").style.background = `rgba(256, 256, 256, 1)`;
-        document.getElementById("header").style.boxShadow = `0px 1px 5px gray`;
-        document.getElementById("header").style.color = `black`;
+        header.style.background = `rgba(256, 256, 256, 1)`;
+        header.style.boxShadow = `0px 1px 5px gray`;
+        header.style.color = `black`;
         document.querySelector("header#header div ul").style.color = `#026e9f`;
     }
     else {
-        document.getElementById("header").style.background = `rgba(256, 256, 256, 0)`;
-        document.getElementById("header").style.boxShadow = `0px 1px 5px transparent`;
-        document.getElementById("header").style.color = `white`;
+        header.style.background = `rgba(256, 256, 256, 0)`;
+        header.style.boxShadow = `0px 1px 5px transparent`;
+        header.style.color = `white`;
         document.querySelector("header#header div ul").style.color = `white`;
     }
 }
